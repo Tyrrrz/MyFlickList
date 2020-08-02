a31e72b1ed94dfd706685e851e19ef537bf416d4
Alexey Golub <tyrrrz@gmail.com>
2020-08-02 16:26:16 +0300

    Fix base url injection in openapi client in app

--- MyFlickList.App/src/infra/api.ts
+++ MyFlickList.App/src/infra/api.ts
@@ -1,8 +1,12 @@
+import _ from 'lodash';
 import * as generated from './api.generated';
 import env from './env';
 
+// NSwag's client is handling the url as a string, so we need to sanitize
+const apiUrl = _.trimEnd(env.apiUrl.toString(), '/');
+
 export default {
-  catalog: new generated.CatalogClient(env.apiUrl.toString()),
+  catalog: new generated.CatalogClient(apiUrl),
   utils: {
     getImageUrl: (imageId: string) => env.getRelativeApiUrl('catalog/images' + imageId)
   }
