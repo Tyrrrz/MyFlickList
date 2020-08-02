d65fdc34573e2238c217690040e2c9cfc4bdd5f3
Alexey Golub <tyrrrz@gmail.com>
2020-08-03 00:31:44 +0300

    Fix Google Analytics

--- MyFlickList.App/src/shared/useTracking.tsx
+++ MyFlickList.App/src/shared/useTracking.tsx
@@ -3,20 +3,19 @@ import GoogleAnalytics from 'react-ga';
 import config from '../infra/config';
 import { useLocation } from 'react-router';
 
-const token = config.secrets.googleAnalyticsToken;
-
-if (token) {
-  GoogleAnalytics.initialize(token, {
-    gaOptions: {
-      sampleRate: 100
-    }
-  });
-}
-
 export default function useTracking() {
+  const token = config.secrets.googleAnalyticsToken;
   const location = useLocation();
 
   useEffect(() => {
-    GoogleAnalytics.pageview(location.pathname + location.search);
-  }, [location]);
+    if (token) {
+      GoogleAnalytics.initialize(token, {
+        gaOptions: {
+          sampleRate: 100
+        }
+      });
+
+      GoogleAnalytics.pageview(location.pathname + location.search);
+    }
+  }, [token, location]);
 }
