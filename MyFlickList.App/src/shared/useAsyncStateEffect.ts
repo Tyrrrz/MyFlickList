b81c36c254fbee697e4035c845a88b29e08deccd
Alexey Golub <tyrrrz@gmail.com>
2020-08-03 20:28:38 +0300

    Fix accidental ddos with useEffect

--- MyFlickList.App/src/shared/useAsyncStateEffect.ts
+++ MyFlickList.App/src/shared/useAsyncStateEffect.ts
@@ -1,13 +1,12 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function useAsyncStateEffect<T>(resolve: () => Promise<T>, deps: React.DependencyList) {
   const [state, setState] = useState<T | undefined>(undefined);
-  const resolveCallback = useCallback(resolve, [resolve]);
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
-    resolveCallback().then(setState);
-  }, [...deps, resolveCallback]);
+    resolve().then(setState);
+  }, deps);
   /* eslint-enable react-hooks/exhaustive-deps */
 
   return state;
