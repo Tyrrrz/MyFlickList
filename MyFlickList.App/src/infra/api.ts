492c8a3f866dae2f832cc7afeffd22e37c792e8c
Alexey Golub <tyrrrz@gmail.com>
2020-08-17 16:56:03 +0300

    Replace lodash

--- MyFlickList.App/src/infra/api.ts
+++ MyFlickList.App/src/infra/api.ts
@@ -1,12 +1,18 @@
-import _ from 'lodash';
 import * as generated from './api.generated';
 import config from './config';
+import { trimEnd } from './utils';
 
 // NSwag's client is handling the url as a string, so we need to sanitize
-const apiUrl = _.trimEnd(config.apiUrl.toString(), '/');
+const apiUrl = trimEnd(config.apiUrl.toString(), '/');
+
+const fetchProvider = {
+  fetch: (url: RequestInfo, init?: RequestInit) => {
+    return fetch(url, init);
+  }
+};
 
 export default {
-  catalog: new generated.CatalogClient(apiUrl),
+  catalog: new generated.CatalogClient(apiUrl, fetchProvider),
   utils: {
     getImageUrl: (imageId: string) => config.getRelativeApiUrl('catalog/images/' + imageId)
   }
--- MyFlickList.App/src/infra/utils.ts
+++ MyFlickList.App/src/infra/utils.ts
@@ -1,3 +1,13 @@
+export function trimEnd(str: string, end: string) {
+  let temp = str;
+
+  while (temp.endsWith(end)) {
+    temp = temp.substr(0, temp.length - end.length);
+  }
+
+  return temp;
+}
+
 export function isAbsoluteUrl(url: string) {
   return /^[a-z][a-z\d+\-.]*:/iu.test(url);
 }
