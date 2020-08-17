091598fa7bad7df686f7bc95b8325387f9757d84
Alexey Golub <tyrrrz@gmail.com>
2020-08-17 16:11:19 +0300

    Refactor CacheDurations

--- MyFlickList.Api/Internal/CacheDurations.cs
+++ MyFlickList.Api/Internal/CacheDurations.cs
@@ -2,6 +2,12 @@
 {
     internal static class CacheDurations
     {
-        public const int OneDay = 60 * 60 * 24;
+        public const int OneSecond = 1;
+
+        public const int OneMinute = 60 * OneSecond;
+
+        public const int OneHour = 60 * OneMinute;
+
+        public const int OneDay = 24 * OneHour;
     }
 }
