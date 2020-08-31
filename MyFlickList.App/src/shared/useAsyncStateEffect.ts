227eda523521dd9a3fce934dbecb68eeb7179569
Alexey Golub <tyrrrz@gmail.com>
2020-08-31 15:37:03 +0300

    Remove condition from useAsyncStateEffect

--- MyFlickList.App/src/shared/useAsyncStateEffect.ts
+++ MyFlickList.App/src/shared/useAsyncStateEffect.ts
@@ -2,18 +2,15 @@ import React, { useEffect, useState } from 'react';
 
 export default function useAsyncStateEffect<T>(
   resolve: () => Promise<T>,
-  deps: React.DependencyList,
-  condition = true
+  deps: React.DependencyList
 ) {
   const [state, setState] = useState<T | undefined>(undefined);
   const [error, setError] = useState<unknown | undefined>(undefined);
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
-    if (condition) {
-      resolve().then(setState, setError);
-    }
-  }, [...deps, condition]);
+    resolve().then(setState, setError);
+  }, deps);
   /* eslint-enable react-hooks/exhaustive-deps */
 
   return [state, error] as const;
